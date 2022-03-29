a4ea0a31765aee1f3306d878fc9e6a4c30ffc073
Rogonoc <8975b09b-034a-4254-90a2-a069b517486e@anonaddy.me>
2022-03-29 11:38:19 +0200

    Tracker is able to fully track buoy - I would say hit/miss ratio of 4/1

--- test2.m
+++ test2.m
@@ -7,12 +7,13 @@ clc;
 hVideoSrc = VideoReader('MAH01462.MP4');
 
 % User input
-roi = [0.500000000000000,0.500000000000000,1440,5.257500000000003e+02]; % Horizontal looking island
-roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 35.500000000000000, 35.500000000000000]; % Initial ROI surrounding buoy; based on first frame
-mThreshold = 1300;
+roi = [0.5,0.5,1440,5.2575e+02];                      % Horizontal looking island + clouds
+roi_buoy_initial = [6.4325e+02, 5.0e+02, 35.5, 35.5]; % Initial ROI surrounding buoy; based on first frame
+roi_buoy_tracker = 15;                                % Size of scanning ROI-region surrounding the KLT-tracker
+mThreshold = 1300;                                    % Strictness of feature extraction for stabilization transforms
 
 % Reset the video source to the beginning of the file.
-%read(hVideoSrc, 1);
+read(hVideoSrc, 1);
 
 % Create video viewer
 hVPlayer = vision.VideoPlayer; 
@@ -25,32 +26,21 @@ imgB = rgb2gray(im2single(readFrame(hVideoSrc)));
 imgBp = imgB;
 
 % Initialize other variables
-trackerAlive = 0;
-trackerWasAlive = 0;
-KLT_points = [];
+trackerAlive = 0;     % State of KLT-tracker (1)
+trackerWasAlive = 0;  % State of KLT-tracker (2)
+KLT_points = [];      % Points spit out by KLT-tracker
+ii = 2;               % Loop variable
+Hcumulative = eye(3); % Initial transformation matrix
 
 % Find features of interest in first frame
-points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy);
-%points = detectMinEigenFeatures(imgBp, 'MinQuality', 0.9, 'ROI', roi_buoy);
-%pointImage = insertMarker(imgBp, points.Location, '+', 'Color', 'white');
-%pointImage = insertShape(pointImage, 'Rectangle', roi_buoy, 'Color','red');
-%figure(2); imshow(pointImage);
-
-% Initialize the tracker
-%initialize(tracker, points.Location, imgBp);
-
-% Starting variable
-ii = 2;
-
-% Initial transformation matrix
-Hcumulative = eye(3);
+points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy_initial);
 
 % ALGORITHM
 % 1. Initialize ROI
 % 2. Wait till FeatureFinder detects point in ROI. If yes, initialize KLT-object
-% 3. Using ROI around returned point of KLT-object, check if the FeatureFinder matches with this point
-% 4. If no point is found in FeatureFinder, the buoy is probably missing. Disable KLT-object
-% 5. Freeze ROI. Go back to step 2
+% 3. Let KLT-tracker do its own thing
+% 4. If no point has been returned by KLT-tracker, the buoy is probably missing. Disable KLT-object
+% 5. Freeze ROI around "dead" point. Go back to step 2
 
 % Start tracking features in video
 while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
@@ -73,12 +63,13 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
     frame = imgBp;
 
     % Check if buoy is detected in ROI using FeatureFinder
-    if (trackerAlive == 0)
+    if (trackerAlive == 0) % Tracker not active
+
         % Detect features using BRISK-algorithm 
-        if (trackerWasAlive == 0)
-            points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.28, 'ROI', roi_buoy);
-        elseif (trackerWasAlive == 1)
-            points = detectBRISKFeatures(imgBp, 'MinQuality', 0.2, 'MinContrast', 0.20, 'ROI', [KLT_points(1) - (rectSize/2), KLT_points(2) - (rectSize/2), 2*rectSize, 2*rectSize]);
+        if (trackerWasAlive == 0)     % Use initial knowledge of ROI around buoy to start tracking it [STRICT]
+            points = detectBRISKFeatures(frame, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy_initial);
+        elseif (trackerWasAlive == 1) % Use previous knowledge of KLT-tracker point to cast new ROI around it [SEMI-STRICT]
+            points = detectBRISKFeatures(frame, 'MinQuality', 0.2, 'MinContrast', 0.2, 'ROI', [KLT_points(1) - floor(roi_buoy_tracker/2), KLT_points(2) - floor(roi_buoy_tracker/2), 2*roi_buoy_tracker, 2*roi_buoy_tracker]);
         end
 
         % Check if points were returned by FeatureFinder in ROI
@@ -99,16 +90,17 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
             trackerAlive = 1;
             trackerWasAlive = 1;
         end
-    elseif (trackerAlive == 1)
+
+    elseif (trackerAlive == 1) % Tracker active
+
         % DON'T INITIALIZE TRACKER AGAIN
         % LET IT DO ITS THING
         
         % Return points of the KLT-tracker
         [KLT_points, validity] = tracker(frame);
 
-        % Check ROI based on current returned point of KLT-tracker
-        rectSize = 15;
-        points = detectBRISKFeatures(imgBp, 'MinQuality', 0.2, 'MinContrast', 0.20, 'ROI', [KLT_points(1) - (rectSize/2), KLT_points(2) - (rectSize/2), 2*rectSize, 2*rectSize]);
+        % Check ROI based on current returned point of KLT-tracker [WEAK]
+        points = detectBRISKFeatures(frame, 'MinQuality', 0.2, 'MinContrast', 0.1, 'ROI', [KLT_points(1) - (roi_buoy_tracker/2), KLT_points(2) - (roi_buoy_tracker/2), 2*roi_buoy_tracker, 2*roi_buoy_tracker]);
 
         % Are there no points returned? --> tracker should not be alive anymore
         if (isempty(KLT_points) || isempty(points))
