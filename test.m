c04b714af6737e48fc965a292f0e88fd23116e1e
Rogonoc <8975b09b-034a-4254-90a2-a069b517486e@anonaddy.me>
2022-03-29 15:09:29 +0200

    Update

--- test.m
+++ test.m
@@ -3,11 +3,19 @@ clear;
 clc;
 
 %% Stabilize
+% ALGORITHM
+% 1. Initialize ROI
+% 2. Wait till feature is detected in ROI. If yes, initialize KLT-object
+% 3. Using ROI around returned point of KLT-object, check if the FeatureFinder matches with this point
+% 4. If no point is found in FeatureFinder, the buoy is probably missing. Disable KLT-object
+% 5. Freeze ROI. Go back to step 2
+
 % Load video to variable
 hVideoSrc = VideoReader('MAH01462.MP4');
 
 % User input
-roi = [0.500000000000000,0.500000000000000,1440,5.257500000000003e+02]; % Horizontal looking island
+roi = [0.500000000000000,0.500000000000000,1440,5.257500000000003e+02];                           % Horizontal looking island + clouds
+roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 35.500000000000000, 35.500000000000000]; % Initial ROI surrounding buoy; based on first frame
 mThreshold = 1300;
 
 % Reset the video source to the beginning of the file.
@@ -16,10 +24,22 @@ read(hVideoSrc, 1);
 % Create video viewer
 hVPlayer = vision.VideoPlayer; 
 
-% Initialize moving and corrected mean
+% Create tracker object
+tracker = vision.PointTracker('MaxBidirectionalError', 40);
+
+% Initialize frames
 imgB = rgb2gray(im2single(readFrame(hVideoSrc)));
 imgBp = imgB;
-correctedMean = imgBp;
+
+% Find features of interest in first frame
+points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy);
+%points = detectMinEigenFeatures(imgBp, 'MinQuality', 0.9, 'ROI', roi_buoy);
+pointImage = insertMarker(imgBp, points.Location, '+', 'Color', 'white');
+pointImage = insertShape(pointImage, 'Rectangle', roi_buoy, 'Color','red');
+figure(2); imshow(pointImage);
+
+% Initialize the tracker
+initialize(tracker, points.Location, imgBp);
 
 % Starting variable
 ii = 2;
@@ -27,7 +47,8 @@ ii = 2;
 % Initial transformation matrix
 Hcumulative = eye(3);
 
-while hasFrame(hVideoSrc) && ii < 3
+% Start tracking features in video
+while hasFrame(hVideoSrc) && ii < 8
     % Read new frame
     imgA = imgB; % z^-1
     frameA = readFrame(hVideoSrc);
@@ -45,11 +66,12 @@ while hasFrame(hVideoSrc) && ii < 3
     
     % Display as color composite with last corrected frame
     %step(hVPlayer, imfuse(imgAp, imgBp, 'ColorChannels', 'red-cyan'));
-    frame = readFrame(hVideoSrc);
-    step(hVPlayer,frame);
+    frame = imgBp;
 
-    % Compute corrected mean
-    correctedMean = correctedMean + imgBp;
+    % Let KLT-object track buoy in specified ROI
+    [points, validity] = tracker(imgBp);
+    out = insertMarker(frame, points(validity, :), '+');
+    step(hVPlayer, out);
 
     % Incrmeent frame counter
     ii = ii + 1;
@@ -76,14 +98,15 @@ title('Corners in A');
 
 % Draw ROI over location of buoy 
 %roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 34.500000000000000, 25.500000000000000];
-roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 34.500000000000000, 25.500000000000000];
+roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 35.500000000000000, 35.500000000000000];
 objectImage = insertShape(imgBp, 'Rectangle', roi_buoy, 'Color','red');
 figure(1); imshow(objectImage)
 
 %%
 
 % Detect points in ROI
-points = detectMinEigenFeatures(imgBp, 'MinQuality', 0.5, 'ROI', roi_buoy);
+points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy);
+%points = detectMinEigenFeatures(imgBp, 'MinQuality', 0.99, 'ROI', roi_buoy);
 %points = detectSURFFeatures(im2gray(imgBp), 'MetricThreshold', 0, 'ROI', roi_buoy);
 pointImage = insertMarker(imgBp, points.Location, '+', 'Color', 'white');
 figure(2); imshow(pointImage);
--- test2.m
+++ test2.m
@@ -6,31 +6,32 @@ clc;
 % Load video to variable
 hVideoSrc = VideoReader('MAH01462.MP4');
 
+% Reset the video source to the beginning of the file.
+read(hVideoSrc, 1);
+
 % User input
 roi = [0.5,0.5,1440,5.2575e+02];                      % Horizontal looking island + clouds
 roi_buoy_initial = [6.4325e+02, 5.0e+02, 35.5, 35.5]; % Initial ROI surrounding buoy; based on first frame
-roi_buoy_tracker = 15;                                % Size of scanning ROI-region surrounding the KLT-tracker
+roi_buoy_tracker = 31;                                % Size of scanning ROI-region surrounding the KLT-tracker
 mThreshold = 1300;                                    % Strictness of feature extraction for stabilization transforms
 
-% Reset the video source to the beginning of the file.
-read(hVideoSrc, 1);
+% Initialize other variables
+trackerAlive = 0;     % State of KLT-tracker (1)
+trackerWasAlive = 0;  % State of KLT-tracker (2)
+KLT_roi_size = 5;     % Block size of KLT-tracker
+KLT_points = [];      % Points spit out by KLT-tracker
+ii = 2;               % Loop variable for frames
+Hcumulative = eye(3); % Initial transformation matrix (stabilization)
 
 % Create video viewer
 hVPlayer = vision.VideoPlayer; 
 
 % Create tracker object
-tracker = vision.PointTracker('MaxBidirectionalError', 1, 'BlockSize', [5 5]);
+tracker = vision.PointTracker('MaxBidirectionalError', 1, 'BlockSize', [KLT_roi_size KLT_roi_size]);
 
 % Initialize frames
 imgB = rgb2gray(im2single(readFrame(hVideoSrc)));
 imgBp = imgB;
-
-% Initialize other variables
-trackerAlive = 0;     % State of KLT-tracker (1)
-trackerWasAlive = 0;  % State of KLT-tracker (2)
-KLT_points = [];      % Points spit out by KLT-tracker
-ii = 2;               % Loop variable
-Hcumulative = eye(3); % Initial transformation matrix
 
 % Find features of interest in first frame
 points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy_initial);
@@ -39,7 +40,7 @@ points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI'
 % 1. Initialize ROI
 % 2. Wait till FeatureFinder detects point in ROI. If yes, initialize KLT-object
 % 3. Let KLT-tracker do its own thing
-% 4. If no point has been returned by KLT-tracker, the buoy is probably missing. Disable KLT-object
+% 4. If no point has been returned by KLT-tracker (or point is invalid), the buoy is probably missing. Disable KLT-object
 % 5. Freeze ROI around "dead" point. Go back to step 2
 
 % Start tracking features in video
@@ -58,8 +59,7 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
     % Warp image
     imgBp = imwarp(imgB, projective2d(Hcumulative), 'OutputView', imref2d(size(imgB)));
     
-    % Display as color composite with last corrected frame
-    %step(hVPlayer, imfuse(imgAp, imgBp, 'ColorChannels', 'red-cyan'));
+    % Save warped image as a frame
     frame = imgBp;
 
     % Check if buoy is detected in ROI using FeatureFinder
@@ -67,9 +67,9 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
 
         % Detect features using BRISK-algorithm 
         if (trackerWasAlive == 0)     % Use initial knowledge of ROI around buoy to start tracking it [STRICT]
-            points = detectBRISKFeatures(frame, 'MinQuality', 0.6, 'MinContrast', 0.3, 'ROI', roi_buoy_initial);
-        elseif (trackerWasAlive == 1) % Use previous knowledge of KLT-tracker point to cast new ROI around it [SEMI-STRICT]
-            points = detectBRISKFeatures(frame, 'MinQuality', 0.2, 'MinContrast', 0.2, 'ROI', [KLT_points(1) - floor(roi_buoy_tracker/2), KLT_points(2) - floor(roi_buoy_tracker/2), 2*roi_buoy_tracker, 2*roi_buoy_tracker]);
+            points = detectBRISKFeatures(frame, 'MinQuality', 0.3, 'MinContrast', 0.3, 'ROI', roi_buoy_initial);
+        elseif (trackerWasAlive == 1) % Use previous knowledge of KLT-tracker point to cast new ROI around it [WEAK](
+            points = detectBRISKFeatures(frame, 'MinQuality', 0.3, 'MinContrast', 0.2, 'ROI', [KLT_points(1) - floor(roi_buoy_tracker/2), KLT_points(2) - floor(roi_buoy_tracker/2), roi_buoy_tracker, roi_buoy_tracker]);
         end
 
         % Check if points were returned by FeatureFinder in ROI
@@ -82,13 +82,16 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
     
             % Insert marker in frame to indicate (potential) buoy
             frame = insertMarker(frame, KLT_points(validity, :), '+', 'Color', 'white');
-    
-            % Convert frame to correct dimensions
-            frame = rgb2gray(im2single(frame));
+            frame = insertShape(frame, 'Rectangle', [KLT_points(1) - floor(KLT_roi_size/2), ...
+                KLT_points(2) - floor(KLT_roi_size/2), KLT_roi_size, KLT_roi_size], 'Color', 'white');
     
             % Indicate that tracker is alive
             trackerAlive = 1;
             trackerWasAlive = 1;
+        elseif (isempty(points) && trackerWasAlive == 1) 
+            % Insert marker in frame to indicate ROI
+            frame = insertShape(frame, 'Rectangle', [KLT_points(1) - floor(roi_buoy_tracker/2), ...
+                KLT_points(2) - floor(roi_buoy_tracker/2), roi_buoy_tracker, roi_buoy_tracker], 'Color', 'white');
         end
 
     elseif (trackerAlive == 1) % Tracker active
@@ -100,21 +103,27 @@ while hasFrame(hVideoSrc) && ii < hVideoSrc.NumFrames
         [KLT_points, validity] = tracker(frame);
 
         % Check ROI based on current returned point of KLT-tracker [WEAK]
-        points = detectBRISKFeatures(frame, 'MinQuality', 0.2, 'MinContrast', 0.1, 'ROI', [KLT_points(1) - (roi_buoy_tracker/2), KLT_points(2) - (roi_buoy_tracker/2), 2*roi_buoy_tracker, 2*roi_buoy_tracker]);
+        points = detectBRISKFeatures(frame, 'MinQuality', 0.2, 'MinContrast', 0.2, 'ROI', [KLT_points(1) - roi_buoy_tracker, KLT_points(2) - roi_buoy_tracker, (2*roi_buoy_tracker + 1), (2*roi_buoy_tracker + 1)]);
+        frame = insertShape(frame, 'Rectangle', [KLT_points(1) - floor(roi_buoy_tracker/2), KLT_points(2) - floor(roi_buoy_tracker/2), roi_buoy_tracker, roi_buoy_tracker], 'Color', 'white');
 
-        % Are there no points returned? --> tracker should not be alive anymore
-        if (isempty(KLT_points) || isempty(points))
+        % Are there no points returned or KLT points are no longer valid? 
+        % --> tracker should not be alive anymore
+        if (isempty(KLT_points) || isempty(points) || (validity == 0))
             release(tracker); % Release the tracker
             trackerAlive = 0; % Disable tracker
         else
             % Insert marker in frame to indicate (potential) buoy
             frame = insertMarker(frame, KLT_points(validity, :), '+', 'Color', 'white');
-
-            % Convert frame to correct dimensions
-            frame = rgb2gray(im2single(frame));
+            frame = insertShape(frame, 'Rectangle', [KLT_points(1) - floor(KLT_roi_size/2), ...
+                KLT_points(2) - floor(KLT_roi_size/2), KLT_roi_size, KLT_roi_size], 'Color', 'white');
         end
     end
-   
+
+    % Convert frame to correct dimensions (dirty fix)
+    if (size(frame, 3) == 3)
+        frame = rgb2gray(im2single(frame));
+    end
+
     % Play current frame in video player
     step(hVPlayer, frame);
 
@@ -124,34 +133,3 @@ end
 
 % Release video viewer
 release(hVPlayer);
-
-%% Test
-
-roi = [0.5, 402, 1042, 90]; % Horizontal looking island
-
-imgA = rgb2gray(im2single(readFrame(hVideoSrc))); % Read first frame into imgA
-pointsA = detectSURFFeatures(imgA, 'MetricThreshold', 2000, 'ROI', roi);
-% Display corners found in images A and B.
-figure; imshow(imgA); hold on;
-plot(pointsA);
-title('Corners in A');
-
-%% Test 2
-
-% show image
-%imshow(imgA)
-
-% Draw ROI over location of buoy 
-%roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 34.500000000000000, 25.500000000000000];
-roi_buoy = [6.432499999999999e+02, 5.00000000000001e+02, 35.500000000000000, 35.500000000000000];
-objectImage = insertShape(imgBp, 'Rectangle', roi_buoy, 'Color','red');
-figure(1); imshow(objectImage)
-
-%%
-
-% Detect points in ROI
-points = detectBRISKFeatures(imgBp, 'MinQuality', 0.6, 'MinContrast', 0.275, 'ROI', roi_buoy);
-%points = detectMinEigenFeatures(imgBp, 'MinQuality', 0.99, 'ROI', roi_buoy);
-%points = detectSURFFeatures(im2gray(imgBp), 'MetricThreshold', 0, 'ROI', roi_buoy);
-pointImage = insertMarker(imgBp, points.Location, '+', 'Color', 'white');
-figure(2); imshow(pointImage);
